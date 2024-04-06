92ad2c01fe2ddbaca83b93ddd25fb9c5a1fe9760
defDimych <def.dimych@gmail.com>
2024-04-06 22:11:58 +0300

    add lesson-03/03

---
+++ lesson-03/03.js
@@ -0,0 +1,5 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+
+const fruits2 = fruits.slice(1, fruits.length - 2);
+
+console.log(fruits2);
